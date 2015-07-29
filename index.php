5281aa0379c8f685061640805c3e4bf3af4674aa
Tarun Garg <tarungarg546@gmail.com>
2015-07-30 00:25:01 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Rule This World!
+		</title>
+	</head>
+	<body>
+		<script src="fb.js">
+		// This function is called when someone finishes with the Login
+  
+</script>
+<fb:login-button scope="public_profile,email, user_birthday, user_hometown, user_location, user_likes, user_activities, user_education_history, user_work_history, user_about_me" onlogin="checkLoginState();">
+</fb:login-button>
+<div id="status"></div>
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+
+	</body>
+</html>
